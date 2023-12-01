9a5b462d68a396cc33ce7f2abb9b01b3b9297df7
seb.hall <sebhalldesigns@gmail.com>
2023-12-01 16:55:52 +0000

    working freetype on linux

--- Windowing/source/uWindowManager.cpp
+++ Windowing/source/uWindowManager.cpp
@@ -16,10 +16,6 @@ std::vector<uWindowStuffForManager> uWindowManager::windows;
 #include <cstdio>
 #include <map>
 
-#ifdef _WIN32
-    const wchar_t CLASS_NAME[]  = L"UniversalUI Window";
-
-    LRESULT CALLBACK Win32WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
     FT_Library ft;
     struct Character {
@@ -30,6 +26,12 @@ std::vector<uWindowStuffForManager> uWindowManager::windows;
     };
 
     std::map<char, Character> Characters;
+
+#ifdef _WIN32
+    const wchar_t CLASS_NAME[]  = L"UniversalUI Window";
+
+    LRESULT CALLBACK Win32WindowProcedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 
 #elif __linux__
     Display *display;
@@ -340,6 +342,63 @@ FT_Done_FreeType(ft);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
         glEnable(GL_TEXTURE_2D);   
         glEnable(GL_POINT_SMOOTH); 
+
+          if (FT_Init_FreeType(&ft)) {
+            // Handle error: initialization failed
+        }
+
+        
+
+        FT_Face face;
+        if (FT_New_Face(ft, "./font.ttf", 0, &face)) {
+            // Handle error: failed to load font
+        }
+
+        // Set the desired font size
+        FT_Set_Pixel_Sizes(face, 0, 13);
+
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+        for (unsigned char c = 0; c < 128; c++)
+        {
+            // load character glyph 
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+            {
+                printf("ERROR::FREETYTPE: Failed to load Glyph\n");
+                continue;
+            }
+            // generate texture
+            unsigned int texture;
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                GL_ALPHA,
+                face->glyph->bitmap.width,
+                face->glyph->bitmap.rows,
+                0,
+                GL_ALPHA,
+                GL_UNSIGNED_BYTE,
+                face->glyph->bitmap.buffer
+            );
+            // set texture options
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            // now store character for later use
+            Character character = {
+                texture, 
+                glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                face->glyph->advance.x
+            };
+            Characters.insert(std::pair<char, Character>(c, character));
+}
+
+        FT_Done_Face(face);
+FT_Done_FreeType(ft);
 
         // Show the window
         XMapWindow(display, window->systemHandle);
@@ -684,6 +743,12 @@ void X11WindowProcedure(XEvent event) {
 
 
                 RenderView(&windowStuff.windowPointer->rootView);
+
+
+                    for (int j = 0;j < 30; j++) {
+                        RenderText("By systematically checking each of these areas, you should be able to identify and correct the issue with the color in your text rendering. If the problem persists, please provide a code snippet or more details about your rendering setup, and I can offer more specific guidance.", 10.0f, 10.0f + 15.0f*(float)j, 1.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+                    }
+                    
                 glXSwapBuffers(display, xWindow);
 
                 return;
