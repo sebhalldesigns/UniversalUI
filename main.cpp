3ad7d9e1d95e04ce5ed1f798e2e84d592be9cdc6
sebhall <sebhalldesigns@gmail.com>
2023-12-02 16:30:21 +0000

    macos and ios build test

--- main.cpp
+++ main.cpp
@@ -15,27 +15,8 @@ class MyApp : public uApplication {
 
     void FinishedLaunching() override {
 
-        printf("started launching!!\n");
+        printf("started launching from macos!!\n");
 
-        /*uWindow* window1 = new uWindow(1000.0, 750.0, "window 1");
-        window1->SetVisibility(uWindowVisibility::VISIBLE);
-        window1->rootView.backgroundColor = { 0.0, 0.0, 0.0, 1.0};
-
-        MyView* view = new MyView();
-        view->backgroundColor = { 1.0, 1.0, 1.0, 1.0};
-        
-        uLayoutNode node;
-        node.frame = { 10.0, 10.0, 250.0, 250.0 };
-
-        window1->rootView.layoutNode->childNodes.push_back(node);
-        view->layoutNode = &window1->rootView.layoutNode->childNodes[0];
-        window1->rootView.subviews.push_back(view);
-        view->layoutNode->AddConstraint(window1->rootView.layoutNode, uLayoutAnchor::TOP, uLayoutAnchor::TOP, 0.0);
-        view->layoutNode->AddConstraint(window1->rootView.layoutNode, uLayoutAnchor::BOTTOM, uLayoutAnchor::BOTTOM, 0.0);
-        view->layoutNode->AddConstraint(window1->rootView.layoutNode, uLayoutAnchor::LEFT, uLayoutAnchor::LEFT, 0.0);
-        view->layoutNode->AddConstraint(window1->rootView.layoutNode, uLayoutAnchor::WIDTH, uLayoutAnchor::WIDTH, 0.0, 1.0);*/
-        
-        printf("finished launching!\n");
     }
 };
 
@@ -49,7 +30,7 @@ public:
 
 int main() {
 
-    uApplication* app = new uApplication();
+    MyApp* app = new MyApp();
     app->simpleWindow = new MyWindow();
     return UniversalUI(app);
 }
